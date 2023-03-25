e76f4c016fa198e42914fd8d4129110fed8d9a26
Andrzej Łuszcz <endriu1297@gmail.com>
2023-03-25 17:55:12 +0100

    add setting date of departure

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const puppeteer = require('puppeteer');
 
+//Because it's polish site there is polish name of months
+const months = [
+  'styczeń',
+  'luty',
+  'marzec',
+  'kwiecień',
+  'maj',
+  'czerwiec',
+  'lipiec',
+  'sierpień',
+  'wrzesień',
+  'październik',
+  'listopad',
+  'grudzień',
+];
+
 async function searchFlights() {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    defaultViewport: { width: 1920, height: 1080 },
+  });
   const page = await browser.newPage();
   await page.goto('https://www.esky.pl/tanie-loty');
   const buttonSelector = '.css-47sehv';
@@ -19,7 +37,28 @@ async function searchFlights() {
   const arrivalAirPort = await page.$('#arrivalRoundtrip0');
   await arrivalAirPort.type('Helsinki');
 
-  await page.screenshot({ path: 'images/screen5.jpg' });
+  const departureDateInput = await page.$('#departureDateRoundtrip0');
+  await departureDateInput.click();
+
+  const departureMonth = await page.$('.ui-datepicker-month');
+  const monthValue = await page.evaluate(
+    (el) => el.textContent,
+    departureMonth,
+  );
+
+  const currentMonthIndex = months.findIndex(
+    (month) => month.toLowerCase() === monthValue.toLowerCase(),
+  );
+
+  for (let i = currentMonthIndex; i < 5; i++) {
+    const nextMonthBtn = await page.$('.ui-datepicker-next');
+    await nextMonthBtn.click();
+  }
+
+  const linkHandlers = await page.$x("//a[contains(text(), '28')]");
+  await linkHandlers[0].click();
+
+  await page.screenshot({ path: 'images/screen6.jpg' });
 
   await browser.close();
 }
